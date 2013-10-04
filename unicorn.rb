a06029b6eee75ce061a0599debe6b020da3a22f3
Meeka <rrgayhart@gmail.com>
2013-10-04 11:53:03 -0600

    Gets unicorn tests to pass

--- unicorn.rb
+++ unicorn.rb
@@ -1,10 +1,25 @@
 class Unicorn
 
-  def initialize(name)
+  def initialize(name, color="white")
     @name = name
+    @color = color
   end
 
   def name
     @name
+  end
+
+  def color
+    @color
+  end
+
+  def white?
+    if @color == "white" then true
+    else false
+    end
+  end
+
+  def say(input)
+    "**;* #{input} **;*"
   end
 end
