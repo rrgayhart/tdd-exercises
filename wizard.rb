47d23daac71790345666c9ff93ef1a1932f42822
Meeka <rrgayhart@gmail.com>
2013-10-04 13:07:26 -0600

    finishes passing all tests

--- wizard.rb
+++ wizard.rb
@@ -1,11 +1,20 @@
 class Wizard
 
-  def initialize(name)
+  def initialize(name, bearded = "true")
     @name = name
+    @bearded = bearded
   end
 
   def name
     @name
+  end
+
+  def bearded?
+    @bearded[0]
+  end
+
+  def incantation(input)
+    "sudo #{input}"
   end
 
 end
