7b51d1bd0fd432e7c46881a793b633cde1f007f5
Meeka <rrgayhart@gmail.com>
2013-10-04 11:59:44 -0600

    Passes vampire tests

--- vampire.rb
+++ vampire.rb
@@ -1,11 +1,25 @@
 class Vampire
 
-def initialize(name)
+def initialize(name, pet="bat")
   @name = name
+  @pet = pet
+  @thirsty = true
 end
 
 def name
   @name
+end
+
+def pet
+  @pet
+end
+
+def drink
+  @thirsty = false
+end
+
+def thirsty?
+  @thirsty
 end
 
 end
