869bf45985ebc2a4cefa073ec2fa7cc62530c2cf
Meeka <rrgayhart@gmail.com>
2013-10-04 11:36:11 -0600

    passes hobbit tests

--- hobbit.rb
+++ hobbit.rb
@@ -1,11 +1,34 @@
 class Hobbit
 
-  def initialize(name)
+  def initialize(name, disposition = "homebody")
     @name = name
+    @disposition = disposition
+    @age = 0
   end
 
   def name
     @name
+  end
+
+  def disposition
+    case @name 
+      when "Samwise"
+       "homebody" 
+      when "Frodo"
+        "adventurous"
+    end
+  end
+
+  def age
+    @age
+  end
+
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def adult?
+    if age > 32 then true end
   end
 
 end
